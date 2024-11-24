c92935c990c7ee2389e0ba955d821c468e0e0cc3
dalia9999 <daliabedrosyan47@gmail.com>
2024-11-24 18:25:59 +0200

    Fixed:  all users visualization

--- Collab Messenger/src/views/Home/Home.jsx
+++ Collab Messenger/src/views/Home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
     useEffect(() => {
         const fetchUserCount = async () => {
             const count = await getAllUsers();
-            setUserCount(count);
+            setUserCount(count.length);
         };
 
         fetchUserCount();
